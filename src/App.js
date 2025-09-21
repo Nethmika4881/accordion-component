686a722726dfe7a5011d43bffe70ecff9061d4c3
Nethmika Kumarasiri <nethmikakumarasiri@gmail.com>
2025-09-21 22:52:15 +0530

    initial stage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Accordion />
+    </>
+  );
+}
+
+function Accordion() {
+  return (
+    <div className="accordion">
+      <AccordionItem />
+      <AccordionItem />
     </div>
   );
 }
 
-export default App;
+function AccordionItem() {
+  return (
+    <div className="item open">
+      {" "}
+      <span className="number">01</span>
+      <span className="title text">Where are these chairs assembled?</span>
+      <span className="icon">&minus;</span>
+      <div className="content-box">
+        <ul>
+          <li>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Accusantium, quaerat temporibus quas dolore provident nisi ut
+            aliquid ratione beatae sequi aspernatur veniam repellendus.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
